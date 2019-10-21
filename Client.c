0f27d2faf0ea6bfb7a4ab6eeedbb9937e4788531
MattiaInnocenti <mattia.innocenti1998@gmail.com>
2019-10-21 16:09:32 +0200

    Update Client!

--- Client.c
+++ Client.c
@@ -11,74 +11,7 @@
 
 int main(int argc, char const *argv[])
 {
-    /*int fdSocket, res;
-
-    fdSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if(fdSocket < 0) {
-        perror("Errore creazione socket stream.");
-        exit(1);
-    }
-
-    struct sockaddr_in clientAddr;
-    clientAddr.sin_port = 0;
-    clientAddr.sin_family = AF_INET;
-
-    struct hostent *localHost;
-    localHost = gethostbyname("127.0.0.1");
-
-    if(localHost == 0){
-        perror("Gethostbyname fallita");
-        exit(1);
-    }
-
-    bcopy(localHost->h_addr_list[0],&clientAddr.sin_addr,localHost->h_length);
-
-    //res = bind(fdSocket,(struct sockaddr*) &clientAddr, sizeof(clientAddr));
-    printf("CLIENT: Ho creato il binding con il dispositivo fisico.\n");
-
-    if(res < 0){
-        perror("Errore binding socket stream.");
-        exit(1);
-    }
-
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_port = 4444;
-    serverAddr.sin_family = AF_INET;
-
-    struct hostent *remoteHost;
-    remoteHost = gethostbyname("192.168.43.134");
-
-    if(remoteHost == 0){
-        perror("Gethostbyname fallita");
-        exit(1);
-    }
-
-    bcopy(remoteHost->h_addr_list[0],&serverAddr.sin_addr,remoteHost->h_length);
-
-    //mi devo connettere alla macchina del server creo una istanza all'IP della macchina che risiede a 
-    //192.168.43.134
-    res = connect(fdSocket, (struct sockaddr*) &serverAddr, sizeof(serverAddr));
-    printf("CLIENT: la connect ha restituito %d\n", res);
-    printf("CLIENT: Ho creato la connessione con il server.\n");
-
-    if(res == -1){
-        perror("Errore instaurazione connessione stream.");
-        exit(1);
-    } else {
-        printf("CLIENT: Siamo connessi con il server!");
-        sleep(500);
-        char *msgCliente = "Siamo finalmente connessi, forse...";
-        int n = write(fdSocket, msgCliente, sizeof(msgCliente));
-        printf("CLIENT: Ho inviato al server %d byte!\n", n);
-        
-        char msgServer[256];
-        int m = read(fdSocket, msgServer, 256);
-        printf("CLIENT: Ho ricevuto dal server %d byte!\n", m);
-
-        printf("CLIENT: ricevuto dal server\n%s\n", msgServer);
-    }
-    close(fdSocket);
-    return 0;*/
+    
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
     char msgToSend[256]; 
@@ -106,10 +39,13 @@ int main(int argc, char const *argv[])
     while((read(0, msgToSend, sizeof(msgToSend))) > 0) {
         write(sock, msgToSend, sizeof(msgToSend));
         printf("CLIENT: %s\n", msgToSend);
+        bzero(msgToSend, sizeof(msgToSend));
 
         read(sock, receiveMsg, sizeof(receiveMsg));
-        printf("SERVER: %s\n", msgToSend);
+        printf("SERVER: %s\n", receiveMsg);
+        bzero(receiveMsg, sizeof(receiveMsg));
     }
     close(sock);
     return 0; 
+
 }
